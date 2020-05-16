272275b700b674f63f73d9034a525be78cfb1572
Sonal Bhoraniya <bhoraniya@bhoraniya-macbookair.roam.corp.google.com>
2020-05-16 12:42:31 -0700

    Updates

---
+++ precache-manifest.f72460ef2966de25e034da9fc029a688.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c38348b33f4097add9ac133b6b6d2001",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "e6981cdfb6163b6836c2",
+    "url": "/clickygame/static/css/main.c90a5edf.chunk.css"
+  },
+  {
+    "revision": "531f56a04778d69f3892",
+    "url": "/clickygame/static/js/2.e5efb325.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clickygame/static/js/2.e5efb325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6981cdfb6163b6836c2",
+    "url": "/clickygame/static/js/main.b17e0a37.chunk.js"
+  },
+  {
+    "revision": "915f4ac814668c12d35e",
+    "url": "/clickygame/static/js/runtime-main.d17c2b74.js"
+  }
+]);
